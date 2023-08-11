f1089eba37d76ad260b8e18bacfe6185e98ecdac
Saurav Paul <sauravpaul03@gmail.com>
2023-08-11 17:02:08 +0530

    var

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const LoginToken string = "askdnkandabsdbnan"
+
 func main() {
 	var username string = "Saurav"
 	fmt.Println(username)
@@ -10,4 +12,32 @@ func main() {
 	var isLoggedIn bool = true
 	fmt.Println(isLoggedIn)
 	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float64 = 255.4646101616016
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	var aVariable int
+	fmt.Println(aVariable)
+	fmt.Printf("Variable is of type: %T \n", aVariable)
+
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	//explicit type
+	var website = "example.com"
+	fmt.Println(website)
+
+	//no var type
+	noOfUser := 500000
+	fmt.Println(noOfUser)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
+
 }
